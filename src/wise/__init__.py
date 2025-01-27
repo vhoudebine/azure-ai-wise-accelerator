76774ec1789238c4e26e38f049538a1409493793
vhoudebine <vhoudebine@microsoft.com>
2025-01-27 14:32:37 -0500

    added python module for post call eval

---
+++ src/wise/__init__.py
@@ -0,0 +1,7 @@
+# wise/__init__.py
+
+from .fact_extractor import FactExtractor
+from .fact_checker import FactChecker
+from .evaluator import Evaluator
+
+__all__ = ["FactExtractor", "FactChecker", "Evaluator"]
